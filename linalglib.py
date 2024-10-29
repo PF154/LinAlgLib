315cbe844b17f7349184ad1eb6d7ca27fc085d92
Peter Forsberg <pwforsberg@wm.edu>
2024-10-29 11:54:23 -0400

    matrix multiplication in progress

--- linalglib.py
+++ linalglib.py
@@ -1,12 +1,7 @@
 class Matrix:
-    def __init__(self, columns, rows):
+    def __init__(self, rows, columns):
         self.matrix = [[0 for i in range(columns)] for i in range(rows)]
-        # change this to
-        # for i in range(rows):
-        #     for j in range(columns):
-        #         new_row = []
-        #         new_row.append = 0
-        self.size = (columns, rows)
+        self.size = (rows, columns)
     
     def determinant(self):
         # For now, we just do a 2x2 determinant. This will be updated later
@@ -15,17 +10,48 @@ class Matrix:
     def __add__(self, other):
         # first check to make sure that matricies are the same size
         if (self.size != other.size):
-            # change this to raise TypeError eventually
-            print("ERROR! Tried to add two matrices of different sizes")
-            return Matrix(3, 3)
+            raise ValueError("Tried to add two matrices of different sizes")
         
         # add by element
-        sum_matrix = Matrix(3, 3)
+        sum_matrix = Matrix(self.size[0], self.size[1])
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 sum_matrix.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
 
         return sum_matrix
+
+    def __sub__(self, other):
+        if (self.size != other.size):
+            raise ValueError("Tried to add two matices of different sizes")
+        
+        # subtract each element respectively
+        difference_matrix = Matrix(self.size[0], self.size[1])
+        for i in range(self.size[0]):
+            for j in range(self.size[0]):
+                difference_matrix.matrix[i][j] = self.matrix[i][j] - other.matrix[i][j]
+        
+        return difference_matrix
+
+    def __mul__(self, other):
+        # make sure that sizes of matrices are compatible
+        if (self.size[1] != other.size[0]):
+            raise ValueError("Tried to multiply matrices of incompatible sizes")
+        
+        product_matrix = Matrix(self.size[0], other.size[1])
+        for i in range(self.size[0]):
+            # i is the index of the row in matrix self
+            # we want to dot prod the row with each column of matrix other.
+            # each dot prod should become a successive element in the first row of product_matrix
+            for j in range(other.size[1]):
+                # j is the index of a column in other
+                dp = 0
+                for k in range(other.size[0]):
+                    # k is the index of an element in column j
+                    dp += self.matrix[i][k] * other.matrix[k][j]
+                product_matrix.matrix[i][j]
+
+        return product_matrix
+
 
     def __str__(self):
         return_string = "["
@@ -62,4 +88,4 @@ for i in range(3):
 
 print(m2)
 
-print(m1 + m2)
+print(m1 * m2)
